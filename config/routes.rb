633464eafb4d73e4d10baace5a83a10fb53303f8
stanley-world <146215090+stanley-world@users.noreply.github.com>
2023-12-04 21:42:44 +0000

    multi

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "addition", :action => "show_addition_form" })
 
-  get("/ad", { :controller => "addition", :action => "show_addition_form" })
+  get("/add", { :controller => "addition", :action => "show_addition_form" })
   get("/wizard_add", { :controller => "addition", :action => "add_these" })
 
-  get("/subtract", { :controller => "subtract", :action => "show_sub_form" })
+  get("/subtract", { :controller => "subtraction", :action => "show_subtraction_form" })
   get("", { :controller => "subtraction", :action => "subtract" })
 
-  get("/multiply", { :controller => "multiplication", :action => "multiplication" })
+  get("/multiply", { :controller => "multiplication", :action => "show_multiplication_form" })
   get("/wizard_multiply", { :controller => "multiplication", :action => "multiply" })
 
   get("/divide", { :controller => "division", :action => "show_division_form" })
